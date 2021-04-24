0939cad80c5bfa901e231f47cffe7596c23db875
anjaporter88 <70259987+anjaporter88@users.noreply.github.com>
2021-04-24 18:38:59 +0100

    Added search engine

--- src/Search.js
+++ src/Search.js
@@ -6,6 +6,7 @@ import "./Search.css";
 export default function Search() {
   const [ready, setReady] = useState(false);
   const [weatherData, setweatherData] = useState({});
+  const [city, setCity] = useState("Brighton");
 
   function handleResponse(response) {
     setweatherData({
@@ -19,18 +20,35 @@ export default function Search() {
     });
     setReady(true);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function search() {
+    const apiKey = "3ac4d291dda277122b73faa304a1df6a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (ready) {
     return (
       <div className="search">
         <div className="row">
           <div className="col-12">
-            <form id="search-engine">
+            <form id="search-engine" onSubmit={handleSubmit}>
               <input
                 type="text"
                 name="city"
                 placeholder="Enter a city..."
                 id="city-input"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
               <button type="button" className="button" id="current-location">
                 <i className="fas fa-map-marker-alt"></i>
@@ -38,7 +56,7 @@ export default function Search() {
               <input
                 type="submit"
                 name="search"
-                value="Get Forecast"
+                value="Get Weather"
                 className="button"
               />
             </form>
@@ -48,11 +66,7 @@ export default function Search() {
         </div>
     );
   } else {
-    const apiKey = "3ac4d291dda277122b73faa304a1df6a";
-    let city = "Brighton";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading weather data...";
   }
 }
